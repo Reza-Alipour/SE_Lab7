62048d90977ab4071cb930e783984b630a91e40b
Reza Alipour <56475612+Reza-Alipour@users.noreply.github.com>
2023-05-22 18:51:01 +0330

    Push MiniJava project

---
+++ src/main/java/Main.java
@@ -0,0 +1,17 @@
+import parser.Parser;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Parser parser = new Parser();
+        try {
+            // start parsing
+            parser.startParse(new Scanner(new File("src/main/resources/code")));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
